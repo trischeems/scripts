8cb0d55e891c769b63609fbaae79d6fe0aa76289
tri3d <minhtri12123@gmail.com>
2025-06-07 18:21:56 +0700

    m

--- tri3dTools/Source/License/reEcn.py
+++ tri3dTools/Source/License/reEcn.py
@@ -2,11 +2,11 @@ import os
 import maya.cmds as cmds
 def read_bin_and_save():
     key = 123
-    current_dir = os.path.dirname('/Users/tri3d/scripts/tri3dTools/Source/License/reEcn.py')
+    current_dir = os.path.dirname('C:/Users/rigging/Documents/maya/2019/scripts/tri3dTools/Source/License/reEcn.py')
     filepath = os.path.join(current_dir, 'data.bin')
     with open(filepath, 'rb') as f:
         data = f.read()
-    #decrypted = ''.join([chr(ord(c) ^ key) for c in data])
-    decrypted = ''.join([chr(int(c) ^ key) for c in list(data)])
+    decrypted = ''.join([chr(ord(c) ^ key) for c in data])
+    #decrypted = ''.join([chr(int(c) ^ key) for c in list(data)])
     cmds.optionVar(sv=('decryptedMessage', decrypted))
 read_bin_and_save() 
